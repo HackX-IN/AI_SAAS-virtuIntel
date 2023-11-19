8ce61da6490799f5c51953fb10b94b5abadd246f
ShivilaDev <138901497+shivillatech1@users.noreply.github.com>
2023-11-19 23:19:01 +0530

    changes

--- components/uses/Heading.tsx
+++ components/uses/Heading.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { LucideIcon } from "lucide-react";
-import { cn } from "../../lib/utils.ts";
+import { cn } from "../../lib/utils";
 
 interface Props {
   title: string;
